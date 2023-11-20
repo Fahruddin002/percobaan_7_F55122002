c66814da3a465bcc852bff875fc1a2a77c64c320
fahruddin002 <faruklebe025@gmail.com>
2023-11-21 00:11:01 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "percobaan 7_F55122002"
+include(":app")
